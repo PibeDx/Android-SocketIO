df800cbfb63bfbf3d6704922f3d403aa75ca22f4
PibeDx <jcuentast@gmail.com>
2017-01-18 18:26:26 -0500

    commit initial

---
+++ Android-SocketIO/app/src/main/java/com/josecuentas/android_socketio/MainActivity.java
@@ -0,0 +1,82 @@
+package com.josecuentas.android_socketio;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    Socket mSocket;
+    String mMessage = "";
+    TextView mTviMessage;
+    EditText mEteMessage;
+    ScrollView mSviContainer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        injectView();
+        injectSocket();
+    }
+
+    private void injectView() {
+        mTviMessage = (TextView) findViewById(R.id.tviMessage);
+        mEteMessage = (EditText) findViewById(R.id.eteMessage);
+        mSviContainer = (ScrollView) findViewById(R.id.sviContainer);
+        findViewById(R.id.butSend).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                String message = mEteMessage.getText().toString();
+                mSocket.emit("chat message", message);
+                mEteMessage.setText("");
+                //ScrollView position bottom end
+                mSviContainer.scrollTo(0, mSviContainer.getBottom());
+            }
+        });
+    }
+
+    private void injectSocket() {
+        try {
+            mSocket = IO.socket("http://169.254.26.0:3000/");
+            mSocket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+                @Override public void call(Object... args) {
+
+                }
+            }).on("chat message", new Emitter.Listener() {
+                @Override public void call(Object... args) {
+                    mMessage += (String) args[0] + "\n";
+                    Log.d(TAG, "call() called with: args = [" + mMessage + "]");
+                    //ejecucion en la UI
+                    runOnUiThread(new Runnable() {
+                        @Override public void run() {
+                            mTviMessage.setText(mMessage);
+                        }
+                    });
+                }
+            }).on(Socket.EVENT_DISCONNECT, new Emitter.Listener() {
+                @Override public void call(Object... args) {
+
+                }
+            });
+            if (!mSocket.connected()) {
+                mSocket.connect();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendMessage() {
+
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+#FUENTE:
+- http://socket.io/get-started/chat/
+- https://github.com/socketio/socket.io-client-java
